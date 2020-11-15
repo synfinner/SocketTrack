2287dfa6e7c106a8aea5f0d9f385a71e554d2e21
synfinner <synfinner@synfinner.ru>
2020-11-15 13:53:22 -0500

    Update ws.js
    Changed some logic. Don't execute if websocket isn't supported; simply return false.

--- files/ws.js
+++ files/ws.js
@@ -1,32 +1,27 @@
 function sTrack() {
 
-// Set up our HTTP request
-var xhr = new XMLHttpRequest();
-
-// Setup our listener to process completed requests
-xhr.onload = function () {
    if ("WebSocket" in window) {
-
-      // Process our return data
-      if (xhr.status >= 200 && xhr.status < 300) {
-         // Runs when the request is successful
-         var IP = xhr.responseText; // Set ip to the response.
-         var ws = new WebSocket("ws://SERVER_IP:8080");
-         ws.onopen = function() {
-            // send ip and paath to server
-            ws.send(JSON.stringify({ip:IP,loc:window.location.pathname}));
-            // close the web socket 
-            ws.close()
-         };
-      } else {
-         return false;
-      }
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://icanhazip.com');
+      xhr.send();
+      xhr.onload = function () {
+         if (xhr.status >= 200 && xhr.status < 300) {
+            // Runs when the request is successful
+            var IP = xhr.responseText; // Set ip to the response.
+            var ws = new WebSocket("ws://SERVER:8080/sockettrack");
+            ws.onopen = function() {
+               // send ip and paath to server
+               ws.send(JSON.stringify({ip:IP,loc:window.location.pathname}));
+               // close the web socket 
+               ws.close()
+            };
+         } else {
+            return false;
+         }
+      };
    } else {
-      return false;
+      return false
    }
-
 };
-xhr.open('GET', 'https://icanhazip.com');
-xhr.send();
-}
+
 sTrack()
