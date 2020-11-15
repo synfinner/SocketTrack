ae164ee3e6bc1131fa2e8898ad66e5f84585a8c5
synfinner <synfinner@synfinner.ru>
2020-11-14 21:53:51 -0500

    Update ws.js

--- files/ws.js
+++ files/ws.js
@@ -10,10 +10,12 @@ xhr.onload = function () {
       // Process our return data
       if (xhr.status >= 200 && xhr.status < 300) {
          // Runs when the request is successful
-         var IP = xhr.responseText;
+         var IP = xhr.responseText; // Set ip to the response.
          var ws = new WebSocket("ws://SERVER_IP:8080");
          ws.onopen = function() {
+            // send ip and paath to server
             ws.send(JSON.stringify({ip:IP,loc:window.location.pathname}));
+            // close the web socket 
             ws.close()
          };
       } else {
